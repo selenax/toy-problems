86be0486ad22be20b5ab3ed8470123281c07093b
Selena Xu <selenawenxu@gmail.com>
2018-02-09 22:20:36 -0800

    Completed

---
+++ codewars /Compare Version.js
@@ -0,0 +1,27 @@
+// console.log(compareVersion('1.0.1', '1.0'), ' => 1.0.1');
+// console.log(compareVersion('2.0.1', '2.0.1'), ' => 2.0.1');
+// console.log(compareVersion('1', '1.0.4'), ' => 1.0.4');
+// console.log(compareVersion('1', '0.0.4'), ' => 1');
+// console.log(compareVersion('20', '2.0.4'), ' => 20'); 
+
+
+
+
+
+var compareVersion = function(v1, v2) {
+    var new1 = v1.split('.');
+    var new2 = v2.split('.');
+    for(var x = 0; x < new1.length; x++) {
+        if(Number(new1[x]) > Number(new2[x])) {
+            return v1;
+        } else if (new2.length > new1.length)
+        return v2;
+    }
+    return v1;
+}
+
+console.log(compareVersion('20', '2.0.4'))
+console.log(compareVersion('1', '0.0.4'))
+console.log(compareVersion('1', '1.0.4'))
+console.log(compareVersion('1.0.1', '1.0'))
+console.log(compareVersion('2.0.1', '2.0.1'))

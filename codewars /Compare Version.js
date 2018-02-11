cc9b90362c4792b161de2a21564fb840a2d7305b
Selena Xu <selenawenxu@gmail.com>
2018-02-10 17:11:41 -0800

    Completed

--- codewars /Compare Version.js
+++ codewars /Compare Version.js
@@ -2,8 +2,7 @@
 // console.log(compareVersion('2.0.1', '2.0.1'), ' => 2.0.1');
 // console.log(compareVersion('1', '1.0.4'), ' => 1.0.4');
 // console.log(compareVersion('1', '0.0.4'), ' => 1');
-// console.log(compareVersion('20', '2.0.4'), ' => 20'); 
-
+// console.log(compareVersion('20', '2.0.4'), ' => 20');  
 
 
 
@@ -11,17 +10,19 @@
 var compareVersion = function(v1, v2) {
     var new1 = v1.split('.');
     var new2 = v2.split('.');
-    for(var x = 0; x < new1.length; x++) {
+    for(var x = 0; x < new1.length; x++) { 
         if(Number(new1[x]) > Number(new2[x])) {
             return v1;
-        } else if (new2.length > new1.length)
+        } else if (new1.length < new2.length) {
         return v2;
+        }
     }
     return v1;
 }
 
-console.log(compareVersion('20', '2.0.4'))
-console.log(compareVersion('1', '0.0.4'))
-console.log(compareVersion('1', '1.0.4'))
+// console.log(compareVersion('20', '2.0.4'))
+// console.log(compareVersion('1', '0.0.4'))
+// console.log(compareVersion('1', '1.0.4'))
+console.log(compareVersion('1.0.2', '1.0')); 
+// console.log(compareVersion('2.0.1', '2.0.1'));
 console.log(compareVersion('1.0.1', '1.0'))
-console.log(compareVersion('2.0.1', '2.0.1'))
---
+++ codewars /Mad Mathematician's Algorithm I (Addition).js
@@ -0,0 +1,10 @@
+
+// Shorten Me: Mad Mathematician's Algorithm I (Addition)
+// Task
+// You have to write a madAdd(a,b) function returning a value of a + b without + in your code.
+
+// Coding Limitation:
+// Less than 30 characters
+
+
+var madAdd = (a,b) => a-(-b);

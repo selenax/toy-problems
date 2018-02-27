17b350fa18df8e4d488d91afff2bb5a89cc71036
Selena Xu <selenawenxu@gmail.com>
2018-02-26 23:59:33 -0800

    Update

--- codewars /Simple consecutive pairs non overlap.js
+++ codewars /Simple consecutive pairs non overlap.js
@@ -1,15 +1,12 @@
 function pairs(ar) {
-  var pair = [];
+  var count = 0;
   for (var x = 0; x < ar.length; x++) {
-    if((ar[x] -1 === ar[x+1] || ar[x] + 1 === ar[x+1])) {
-        if(!pair.includes(ar[x])) {
-        console.log(ar[x])
-        pair.push(ar[x],ar[x+1])
+    if(Math.abs(ar[x]-ar[x+1] === 1)) {
+      count++;
+      }
     }
+    return count;
   }
-}
-  return pair.length/2;
-}
 
 console.log(pairs([1,2,5,8,-4,-3,7,6,5])) 
 console.log(pairs([81, 44, 80, 26, 12, 27, -34, 37, -35])); //2

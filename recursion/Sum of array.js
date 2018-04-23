16a67b3c1c073d38369d8b0dbc3a2782fe14b764
selenax <selenawenxu@gmail.com>
2018-04-23 10:38:58 -0700

    Updated

--- recursion/Sum of array.js
+++ recursion/Sum of array.js
@@ -1,22 +1,33 @@
-// 2. Compute the sum of an array of integers with recursion 
+//Compute the sum of an array of integers with recursion 
 
+
+
+//requirements
+// should return a number ‣
+// should return the sum of an array of non-negative integers ‣
+// should return the sum of an array of negative integers ‣
+// should return the sum of an array of mixed non-negative and negative integers ‣
+// should return 0 for empty array ‣
+// should accept an array with a single integer ‣
+// should not mutate the input array ‣
+// should use recursion by calling self ‣
 
 
 // sum([1,2,3,4,5,6]); // 21
-
-
 var sum = function(array) {
-    if(array.length === 1) {
-        return array[array.length-1]
+    var arr = array.slice()
+    if(!arr[0]) {
+        return 0;
     }
-    return array.pop() + sum(array);
+    if(arr.length === 1) {
+        return arr[0];
+    }
+    return arr.pop() + sum(arr);
 }
 
 
-console.log(sum([1,2,3,4,5,6])); // 21
 
-
-
+//refactor with extra parameter 
 var sum = function(array, x=0) {
     if(x === array.length-1) {
         return array[x];

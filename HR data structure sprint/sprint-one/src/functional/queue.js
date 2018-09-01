e37e0405e8930b8459f0ebc9ae709ad5c7dc9c47
selenax <selenawenxu@gmail.com>
2018-08-31 22:31:49 -0700

    Completed Functional Queue

--- HR data structure sprint/sprint-one/src/functional/queue.js
+++ HR data structure sprint/sprint-one/src/functional/queue.js
@@ -1,22 +1,32 @@
+//FIFO
+
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var counter = 0;
+  var key = 0;
+  var firstItem = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    storage[value] = value;
-    counter++;
+    storage[key] = value;
+    key++;
+
   };
 
   someInstance.dequeue = function() {
-    
+    if (someInstance.size() <= 0) { return; }
+    var removeItem = storage[firstItem];
+    delete storage[firstItem];
+    firstItem++;
+    return removeItem;
+
   };
 
   someInstance.size = function() {
+    return key - firstItem;
   };
 
   return someInstance;

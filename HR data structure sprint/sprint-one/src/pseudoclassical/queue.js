7e1ecc103ac3a19c4c6fa10af86985a23efeab54
selenax <selenawenxu@gmail.com>
2018-09-01 17:29:41 -0700

    WIP queue

--- HR data structure sprint/sprint-one/src/pseudoclassical/queue.js
+++ HR data structure sprint/sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,12 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not not reference your old code in writing the new style.\
+  this.storage = {};
+  this.key = 0;
+  this.firstItem = 0;
+
 };
 
-
+Queue.prototype.enqueue = function(value) {
+  this.
+}

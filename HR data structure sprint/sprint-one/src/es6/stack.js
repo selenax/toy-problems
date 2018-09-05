285b114d15e0374f70434623835e24d883e585cb
selenax <selenawenxu@gmail.com>
2018-09-04 21:58:37 -0700

    Completed es6 stack

--- HR data structure sprint/sprint-one/src/es6/stack.js
+++ HR data structure sprint/sprint-one/src/es6/stack.js
@@ -1,7 +1,22 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.key = 0;
   }
 
+  push(value) {
+    this.storage[this.key] = value;
+    this.key++;
+  }
+
+  pop() {
+    this.size() && this.key--;
+    var removeItem = this.storage[this.key];
+    delete this.storage[this.key];
+    return removeItem;
+  }
+  
+  size() {
+    return this.key;
+  }
 }

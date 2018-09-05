29e6680edefe6a21dd09429b139debe9c1f51de3
selenax <selenawenxu@gmail.com>
2018-09-04 21:56:52 -0700

    Completed es6 queue

--- HR data structure sprint/sprint-one/src/es6/queue.js
+++ HR data structure sprint/sprint-one/src/es6/queue.js
@@ -1,7 +1,24 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.first = 0;
+    this.last = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.last] = value;
+    this.last++;
+  }
+
+  dequeue() {
+
+    var removeItem = this.storage[this.first];
+    delete this.storage[this.first];
+    this.size() && this.first++;
+    return removeItem;
+  }
+
+  size() {
+    return this.last - this.first;
+  }
 }

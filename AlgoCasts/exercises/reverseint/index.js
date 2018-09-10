2cb9c55659a7bf419a8bb836ae5e47123730f126
selenax <selenawenxu@gmail.com>
2018-09-09 20:52:58 -0700

    Completed reverseInt

--- AlgoCasts/exercises/reverseint/index.js
+++ AlgoCasts/exercises/reverseint/index.js
@@ -8,6 +8,33 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let reverse = '';
+  if(n < 0) {
+    let str = n.toString();
+    for(var x = 1; x < str.length; x++) {
+      reverse = reverse + str[str.length - x];
+      console.log(reverse)
+    }
+    return reverse * -1
+  } else {
+    str = n.toString().split('').reverse().join('')
+  }
+  return Number(str);
+}
+
+//refactor
+function reverseInt(n) {
+  const reversed = n
+  .toString()
+  .split('')
+  .reverse()
+  .join('');
+  console.log(reversed)
+  return parseInt(reversed) * Math.sign(n);
+}
+
+
+console.log(reverseInt(-90))
 
 module.exports = reverseInt;

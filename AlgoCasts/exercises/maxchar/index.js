83fa666b33e5aa17e8700f3e2a6034b84414a901
selenax <selenawenxu@gmail.com>
2018-09-09 22:16:51 -0700

    Completed maxChar

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {};
+  let max = 0;
+  let maxChar = '';
+  for (let char of str) {
+    if (obj[char]) {
+      obj[char]++;
+    } else {
+      obj[char] = 1;
+    }
+  }
+  for (let key in obj) {
+    if (obj[key] > max) {
+      max = obj[key];
+      maxChar = key;
+    }
+  }
+  return maxChar;
+}
+
+console.log(maxChar('abcccccccd'));
 
 module.exports = maxChar;

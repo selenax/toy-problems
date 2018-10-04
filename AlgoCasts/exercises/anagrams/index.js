1a6dec971d673da35829d16017673db87b783885
selenax <selenawenxu@gmail.com>
2018-10-03 22:10:28 -0700

    Completed

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// ignore uppercases, punctuations, spaces
+
+function anagrams(stringA, stringB) {
+
+  const str1 = count(stringA);
+  const str2 = count(stringB);
+  if(Object.keys(str1).length !== Object.keys(str2).length) {
+    return false;
+  }
+  for (let key in str1) {
+    if (str1[key] !== str2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function count(str) {
+  let obj = {};
+  for (const x of str.replace(/[^\w]/g, '').toLowerCase()) {
+    obj[x] = obj[x] + 1 || 1;
+  }
+  return obj;
+}
+
+
+//refactor
+function anagrams(stringA, stringB) {
+  return sortChar(stringA) === sortChar(stringB);
+}
+
+function sortChar(str) {
+  return str.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('');
+}
+
+
+
+
+console.log(sortChar('ad!gef'))
+console.log(count('beebee'))
+console.log(anagrams('rail safety', 'fairy tales'));
 
 module.exports = anagrams;

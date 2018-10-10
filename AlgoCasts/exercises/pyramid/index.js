10993a0867f3595280866847035b25d927ca4fb0
selenax <selenawenxu@gmail.com>
2018-10-09 19:01:09 -0700

    Completed

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -14,25 +14,46 @@
 //       ' ### '
 //       '#####'
 
+// n = row
+// ' ' = n-(1)
+// ' ' = n-(2)
+function pyramid(n) {
+  const median = Math.floor((n + (n - 1)) / 2);
+  for (let row = 0; row < n; row++) {
+    let result = '';
 
-function pyramid(n, row = 0, level = '') {
-  if (row === n) {
-    return;
+    for (let column = 0; column < n + (n - 1); column++) {
+      if (median - row <= column && median + row >= column) {
+        result += '#';
+      } else {
+        result += ' ';
+      }
+    }
+    console.log(result);
   }
-
-  if (level.length === 2 * n - 1) {
-    console.log(level);
-    return pyramid(n, row + 1);
-  }
-
-  const midpoint = Math.floor((2 * n - 1) / 2);
-  let add;
-  if (midpoint - row <= level.length && midpoint + row >= level.length) {
-    add = '#';
-  } else {
-    add = ' ';
-  }
-  pyramid(n, row, level + add);
 }
+
+// refactor
+
+function pyramid(n, row = 0, result = '') {
+  const median = Math.floor((n * 2 - 1) / 2);
+  // base case
+  if (row === n) { return; }
+  if (result.length === (n * 2 - 1)) { 
+    console.log(result);
+    return pyramid(n, row+1);
+  }
+  if(median - row <= result.length && median + row >= result.length) {
+    result += '#';
+  } else {
+    result += ' ';
+  }
+  pyramid(n, row, result);
+}
+
+
+
+console.log(pyramid(3));
+
 
 module.exports = pyramid;

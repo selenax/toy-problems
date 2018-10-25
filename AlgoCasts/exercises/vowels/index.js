01ce6e55cd7c7de063635f95e4fc48fcf669c37b
selenax <selenawenxu@gmail.com>
2018-10-24 20:07:06 -0700

    Completed

--- AlgoCasts/exercises/vowels/index.js
+++ AlgoCasts/exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  var vowel = /[aeiouAEIOU]/g;
+  var result = str.match(vowel);
+  if (result === null) { 
+    return 0;
+  }
+  return result.length;
+}
+
+// vowels('Why?') 
 
 module.exports = vowels;

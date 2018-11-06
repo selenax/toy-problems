a7dad5c10fc1d70081544a0fcd7b5c79dd41b543
selenax <selenawenxu@gmail.com>
2018-11-05 20:13:12 -0800

    Completed

--- AlgoCasts/exercises/stack/index.js
+++ AlgoCasts/exercises/stack/index.js
@@ -9,7 +9,22 @@
 //   s.push(2);
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
+class Stack {
+  constructor() {
+    this.data = [];
+  }
 
-class Stack {}
+  push(record) {
+    this.data.push(record);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
 
 module.exports = Stack;

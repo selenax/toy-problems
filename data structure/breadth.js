47b74bfe2d1da8d93885dfd8c363268f89e60dfd
selenax <selenawenxu@gmail.com>
2018-11-26 23:16:13 -0800

    Completed

--- data structure/breadth.js
+++ data structure/breadth.js
@@ -24,3 +24,23 @@ class Tree {
     }
   }
 }
+
+
+Tree.prototype.traverse = function (callback) {
+  var queue=[this];
+  var n;
+
+  while(queue.length>0) {
+
+    n = queue.shift();
+    callback(n.value);
+
+    if (!n.children) {
+      continue;
+    }
+
+    for (var i = 0; i< n.children.length; i++) {
+       queue.push(n.children[i]);
+    }
+  }
+}

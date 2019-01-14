56d00789d53f92c3d99220c8989ddb6fc7c69883
selenax <selenawenxu@gmail.com>
2019-01-13 23:57:45 -0800

    Refactor

--- leetcode/SteamrollArray.js
+++ leetcode/SteamrollArray.js
@@ -14,8 +14,16 @@ function steamrollArray(arr) {
   return result;
 }
 
-console.log(steamrollArray([1, [2], [3, [[4]]]]));
+//refactor
+function steamrollArray(arr) {
+  let flatten = [].concat(...arr);
+  return flatten.some(Array.isArray) ? steamrollArray(flatten) : flatten;
 
+  console.log(flatten);
+}
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+console.log(steamrollArray([[['a']], [['b']]]));
 // steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
 // steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
 // steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].

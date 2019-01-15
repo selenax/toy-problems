4c4daea4c849e5d3a7de546db937cbf177c7a589
selenax <selenawenxu@gmail.com>
2019-01-14 20:46:15 -0800

    Completed + refactored

---
+++ leetcode/firstDuplicate.js
@@ -0,0 +1,42 @@
+// Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+
+// Example
+
+// For a = [2, 1, 3, 5, 3, 2], the output should be firstDuplicate(a) = 3.
+
+// There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
+
+// For a = [5, 5], the output should be firstDuplicate(a) = 5;
+
+// For a = [2, 4, 3, 5, 1], the output should be firstDuplicate(a) = -1.
+
+console.time();
+function firstDuplicate(a) {
+  let check = [];
+  for (let x of a) {
+    if (!check.includes(x)) {
+      check.push(x);
+    } else {
+      return x;
+    }
+  }
+  return -1;
+}
+console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+console.timeEnd();
+
+//refactor - got rid of 2nd loop
+console.time();
+function firstDuplicate(a) {
+  let check = {};
+  for (let x of a) {
+    if (check[x]) {
+      return x;
+    } else {
+      check[x] = 1;
+    }
+  }
+  return -1;
+}
+console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+console.timeEnd();

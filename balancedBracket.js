b23496f5762bc6058714aa2ed18d110b846a83d5
selenax <selenawenxu@gmail.com>
2019-02-26 21:43:11 -0800

    Completed

---
+++ balancedBracket.js
@@ -0,0 +1,91 @@
+// A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+
+// Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {}, and ().
+
+// A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single, unbalanced closing square bracket, ].
+
+// By this logic, we say a sequence of brackets is balanced if the following conditions are met:
+
+// It contains no unmatched brackets.
+// The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
+// Given  strings of brackets, determine whether each sequence of brackets is balanced. If a string is balanced, return YES. Otherwise, return NO.
+
+// Sample Input
+//3
+// {[()]}
+// {[(])}
+// {{[[(())]]}}
+
+// Sample Output
+// YES
+// NO
+// YES
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the isBalanced function below.
+function isBalanced(str) {
+  var stack = [];
+  for (var i = 0; i < str.length; i++) {
+    if (char[stack[stack.length - 1]] === str[i]) {
+      stack.pop();
+    } else {
+      stack.push(string[i]);
+    }
+  }
+
+  return !stack.length;
+}
+
+var char = {
+  "(": ")",
+  "{": "}",
+  "[": "]"
+};
+
+// function isChar(char) {
+//     var str = '(){}[]';
+//     return str.indexOf(char) !== 1;
+
+// }
+// console.log(isChar('['))
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const s = readLine();
+
+        let result = isBalanced(s);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}

5a009d27090dbf6588757bf7978e0310332a08b3
selenax <selenawenxu@gmail.com>
2019-03-27 23:59:42 -0700

    Completed

---
+++ memoize.js
@@ -0,0 +1,14 @@
+const memoize = function(cb) {
+  const cache = {};
+  return function(...args) {
+    //if arg had been called before, return value
+    if (cache[args]) {
+      return cache[args];
+    }
+    //else if arg hasn't been called yet
+    const result = fn.apply(this, args);
+    //store result in cache object
+    cache[args] = result;
+    return result;
+  };
+};
